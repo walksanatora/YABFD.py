652850791af96dbdc72c471b2f3467d66b48ce14
walksanatora <walkerffo22@hotmail.com>
2022-04-29 21:25:08 -0700

    allow YABFD to take argv as code if it is not a file

--- YABFD.py
+++ YABFD.py
@@ -1,11 +1,13 @@
 import YABFD
 from os.path import exists
 import sys
+executor = YABFD.Brainfuck()
+
 if len(sys.argv) < 2:
 	print (f"usage:\n{sys.argv[0]} <code.bf>")
 	exit(1)
 elif exists(sys.argv[1]):
-	executor = YABFD.Brainfuck()
 	executor.execute(sys.argv[1])
 else:
-	print(f"file: '{sys.argv[1]}' does not exist")
+	print(f"evaulating sys.argv[1:] as brainfuck ({sys.argv[1:]})")
+	executor.evaluate(sys.argv[1:])
